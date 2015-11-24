1d78b2459968703123d0b72628224409615b06de
luucaslfs <lfs@cin.ufpe.br>
2015-11-24 11:49:21 -0200

    Classe de testes

--- src/dados/repositorios/TesteLista.java
+++ src/dados/repositorios/TesteLista.java
@@ -5,7 +5,7 @@ import negocios.classesBasicas.*;
 public class TesteLista {
 	
 	public static void main(String[] args) {
-		RepositorioProdutosList listaProdutos = new RepositorioProdutosList("Lista de Produtos");
+		RepositorioProdutosLista listaProdutos = new RepositorioProdutosLista("Lista de Produtos");
 		
 		Produto sapochocolate = new Guloseimas("Sapinhos de Choc", "666", "Sapinhos", "Chocolade Dããã");
 		Produto travessura = new Travessuras("Orelhas Extensiveis", "555", "Ouvir legalzinho", 2, 11);
@@ -17,7 +17,15 @@ public class TesteLista {
 		
 		listaProdutos.remover("555");
 		
-		System.out.println(listaProdutos.toString());
+		Produto aux = listaProdutos.procurar("777");
+		System.out.printf("\nOi, eu sou o goku! \n%s", aux);
+		
+		Produto kitMataAula = new Travessuras("Kit mata aula", "777", "Pra matar Ricardo, ops, a aula de Ricardo* :D", 0, 0);
+		listaProdutos.atualizar("777", kitMataAula);
+		
+		System.out.println("\n\n" + listaProdutos.procurar("777") + "\n");
+		
+		//System.out.println(listaProdutos.toString());
 	}
 
 }
