a007de980785727f26062b3674a62eb6719abb64
luucaslfs <lfs@cin.ufpe.br>
2015-11-24 01:56:32 -0200

    toString()

--- src/negocios/classesBasicas/Travessuras.java
+++ src/negocios/classesBasicas/Travessuras.java
@@ -1,16 +1,16 @@
 package negocios.classesBasicas;
 
 public class Travessuras extends Produto {
-	
+
 	private double nivelPericulosidade;
 	private int censura;
-	
+
 	public Travessuras(String nome, String codigo, String descricao, double nivelPericulosidade, int censura) {
 		super(nome, codigo, descricao);
 		this.nivelPericulosidade = nivelPericulosidade;
 		this.censura = censura;
 	}
-	
+
 	public double getNivelPericulosidade() {
 		return nivelPericulosidade;
 	}
@@ -27,10 +27,15 @@ public class Travessuras extends Produto {
 		this.censura = censura;
 	}
 
-	public void venda() {
-		
-		
+	public String toString() {
+		String resposta = "";
+		resposta = "Nome: " + this.getNome() + "\nCodigo: " + this.getCodigo() + "\nDescricao: " + this.getDescricao()
+				+ "\nNivel de Periculosidade: " + this.getNivelPericulosidade() + "\nCensura: " + this.getCensura();
+		return resposta;
 	}
-	
+
+	public void venda() {
+
+	}
 
 }
