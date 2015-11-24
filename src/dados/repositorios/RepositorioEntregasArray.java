e17f1f321ed97f59039ffba917e4c164c2932a96
lfs <lfs@g3c19.windows.cin.ufpe.br>
2015-11-24 16:56:15 -0300

    Repositorio de Entregas Array

--- src/dados/repositorios/RepositorioEntregasArray.java
+++ src/dados/repositorios/RepositorioEntregasArray.java
@@ -1,10 +1,12 @@
 package dados.repositorios;
 
+import negocios.classesBasicas.Cliente;
 import negocios.classesBasicas.Entrega;
+import negocios.classesBasicas.Produto;
 import negocios.exceptions.EntregaNaoEncontradaException;
 
 public class RepositorioEntregasArray implements RepositorioEntregas {
-	
+
 	private Entrega[] repositorioEntregas;
 	private int indice;
 
@@ -12,21 +14,65 @@ public class RepositorioEntregasArray implements RepositorioEntregas {
 		this.repositorioEntregas = new Entrega[100];
 		this.indice = 0;
 	}
-	
+
 	public void inserir(Entrega entrega) {
-		
+
+		if(indice > this.repositorioEntregas.length) {
+			Entrega[] aux = new Entrega[repositorioEntregas.length * 2];
+			for(int i = 0; i < indice; i++) {
+				aux[i] = this.repositorioEntregas[i];
+			}
+			aux[indice] = entrega;
+		} else {
+			this.repositorioEntregas[indice] = entrega;
+		}
+		indice++;
 	}
 
 	public Entrega procurar(String id) throws EntregaNaoEncontradaException {
-		return null;
+		Entrega resposta = null;
+		int indice = this.buscarCodigo(id);
+		if(indice > -1) {
+			resposta = this.repositorioEntregas[indice];
+		}
+		return resposta;
 	}
 
-	public void atualizar(String id) {
-		
+	public void atualizar(String id, Entrega entrega) {
+		int indice = this.buscarCodigo(id);
+		if(indice != -1){
+			this.repositorioEntregas[indice] = entrega;
+		}
+
 	}
 
 	public void remover(String id) throws EntregaNaoEncontradaException {
-		
+		int b = this.buscarCodigo(id);
+		if( b >= 0){
+			for (int i = 0; i < indice; i++) {
+				Entrega aux = this.repositorioEntregas[i];
+				if (aux.getId().equals(id)) {
+					this.repositorioEntregas[i] = this.repositorioEntregas[indice--];
+					indice = indice--;
+
+				}
+			}
+		}
 	}
+
+	private int buscarCodigo(String id){
+		int resposta = -1;
+		boolean achou = false;
+		for (int i = 0; i < this.indice && !achou; i++) {
+			Entrega aux = this.repositorioEntregas[i];
+			if (aux.getId().equalsIgnoreCase(id)) {
+				resposta = i;
+				achou = true;
+			}
+		}
+		return resposta;
+
+	}
+
 
 }
