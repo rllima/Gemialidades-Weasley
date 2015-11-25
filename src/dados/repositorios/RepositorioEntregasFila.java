a400a912a961441d9973fc4e78b905fcc27c9276
luucaslfs <lfs@cin.ufpe.br>
2015-11-25 10:38:35 -0200

    Repositorio de entregas em fila

---
+++ src/dados/repositorios/RepositorioEntregasFila.java
@@ -0,0 +1,97 @@
+package dados.repositorios;
+
+import negocios.classesBasicas.Entrega;
+import negocios.exceptions.EntregaNaoEncontradaException;
+
+public class RepositorioEntregasFila implements RepositorioEntregas {
+
+	private Node<Entrega> primeiroNo;
+	private Node<Entrega> ultimoNo;
+
+	public RepositorioEntregasFila() {
+		this.primeiroNo = null;
+		this.ultimoNo = null;
+	}
+
+	// Insere no final da fila
+	public void inserir(Entrega entrega) {
+		if(isEmpty()) {
+			primeiroNo = ultimoNo = new Node<Entrega>(entrega);
+		} else {
+			Node<Entrega> aux = ultimoNo;
+			Node<Entrega> novo = new Node<Entrega>(ultimoNo, entrega, null);
+			ultimoNo = novo;
+			if (aux == null){
+				primeiroNo = novo;
+			} else {
+				aux.setProximo(novo);
+			}
+		}
+	}
+
+	// Recebe ID e retorna objeto referente ao id, caso exista
+	public Entrega procurar(String id) /* throws EmptyListException, EntregaNaoEncontradaException*/ {
+		Entrega resposta = null;
+		boolean achou = false;
+		Node<Entrega> atual = primeiroNo;
+		while(atual != null && achou == false) {
+			if(atual.getDado().getId().equals(id)){
+				resposta = atual.getDado();
+				achou = true;
+			}
+			atual = atual.getProximo();
+		}
+		return resposta;
+	}
+
+	public void atualizar(String id, Entrega entrega) {
+		Node<Entrega> atual = this.procurarNode(id);
+		atual.setDado(entrega);
+	}
+
+	public Entrega remover(){
+		Entrega removida = primeiroNo.getDado();
+		
+		if(primeiroNo == ultimoNo) {
+			primeiroNo = ultimoNo = null;
+		} else {
+			primeiroNo = primeiroNo.getProximo();
+		}
+		
+		return removida;
+	}
+
+	public boolean existe(Entrega entrega) {
+		Node<Entrega> atual = this.primeiroNo;
+
+		while(atual != null) {
+			if(atual.getDado().equals(entrega)) {
+				return true;
+			}
+			atual = atual.getProximo();
+		}
+		return false;
+	}
+
+	// Retorna true se a lista esta vazia. (Se o primeiro no esta vazio, a lista esta vazia.
+	public boolean isEmpty() {
+		return primeiroNo == null;
+	}
+
+	// Retorna o no em que se encontra o produto do codigo passado
+	private Node<Entrega> procurarNode(String id)/* throws EmptyListException*/ {
+		Node<Entrega> resposta = null;
+		boolean achou = false;
+		Node<Entrega> atual = primeiroNo;
+		while(atual != null && achou == false) {
+			if(atual.getDado().getId().equals(id)){
+				resposta = atual;
+				achou = true;
+			}
+			atual = atual.getProximo();
+		}
+		return resposta;
+	}
+
+
+}
