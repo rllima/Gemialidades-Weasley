5afbbbe978223f90867418b651fc788c8d9e28fc
luucaslfs <lfs@cin.ufpe.br>
2015-12-08 14:42:20 -0300

    Adicoes no painel de ADMIN

--- src/gui/TelaAdmin.java
+++ src/gui/TelaAdmin.java
@@ -38,11 +38,16 @@ import java.awt.CardLayout;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JFormattedTextField;
+import javax.swing.JTextField;
 
 public class TelaAdmin extends JFrame {
     private static GemialidadesLoja loja;
 
 	private JPanel jPanel1;
+	private JTextField tf_RmvProdutoID;
+	private JTextField tf_RmvClienteID;
+	private JTextField tf_AttClienteID;
+	private JTextField tf_AttProdID;
 
 	/**
 	 * Launch the application.
@@ -81,6 +86,98 @@ public class TelaAdmin extends JFrame {
 		jPanel1.add(panel);
 		panel.setVisible(false);
 		panel.setLayout(null);
+		
+		final Panel panel_AttProduto = new Panel();
+		panel_AttProduto.setLayout(null);
+		panel_AttProduto.setBackground(Color.CYAN);
+		panel_AttProduto.setBounds(0, 0, 358, 235);
+		panel.add(panel_AttProduto);
+		
+		tf_AttProdID = new JTextField();
+		tf_AttProdID.setColumns(10);
+		tf_AttProdID.setBounds(123, 100, 128, 20);
+		panel_AttProduto.add(tf_AttProdID);
+		
+		JLabel label_8 = new JLabel("ID");
+		label_8.setBounds(91, 103, 22, 14);
+		panel_AttProduto.add(label_8);
+		
+		JLabel lblDigiteAbaixoO_3 = new JLabel("Digite abaixo o ID do produto que deseja atualizar");
+		lblDigiteAbaixoO_3.setBounds(63, 29, 240, 14);
+		panel_AttProduto.add(lblDigiteAbaixoO_3);
+		
+		JButton btnAttProd = new JButton("Remover");
+		btnAttProd.setBounds(141, 175, 89, 23);
+		panel_AttProduto.add(btnAttProd);
+		
+		final Panel panel_AttCliente = new Panel();
+		panel_AttCliente.setLayout(null);
+		panel_AttCliente.setBackground(Color.CYAN);
+		panel_AttCliente.setBounds(0, 0, 358, 235);
+		panel.add(panel_AttCliente);
+		
+		tf_AttClienteID = new JTextField();
+		tf_AttClienteID.setColumns(10);
+		tf_AttClienteID.setBounds(123, 100, 128, 20);
+		panel_AttCliente.add(tf_AttClienteID);
+		
+		JLabel label_7 = new JLabel("ID");
+		label_7.setBounds(91, 103, 22, 14);
+		panel_AttCliente.add(label_7);
+		
+		JLabel lblDigiteAbaixoO_2 = new JLabel("Digite abaixo o ID do cliente que deseja atualizar");
+		lblDigiteAbaixoO_2.setBounds(63, 29, 240, 14);
+		panel_AttCliente.add(lblDigiteAbaixoO_2);
+		
+		JButton btnAttClien = new JButton("Remover");
+		btnAttClien.setBounds(141, 175, 89, 23);
+		panel_AttCliente.add(btnAttClien);
+		
+		final Panel panel_RmvCliente = new Panel();
+		panel_RmvCliente.setLayout(null);
+		panel_RmvCliente.setBackground(Color.CYAN);
+		panel_RmvCliente.setBounds(0, 0, 358, 235);
+		panel.add(panel_RmvCliente);
+		
+		tf_RmvClienteID = new JTextField();
+		tf_RmvClienteID.setColumns(10);
+		tf_RmvClienteID.setBounds(123, 100, 128, 20);
+		panel_RmvCliente.add(tf_RmvClienteID);
+		
+		JLabel label_6 = new JLabel("ID");
+		label_6.setBounds(91, 103, 22, 14);
+		panel_RmvCliente.add(label_6);
+		
+		JLabel lblDigiteAbaixoO_1 = new JLabel("Digite abaixo o ID do cliente que deseja remover");
+		lblDigiteAbaixoO_1.setBounds(63, 29, 240, 14);
+		panel_RmvCliente.add(lblDigiteAbaixoO_1);
+		
+		JButton btnRemoverClien = new JButton("Remover");
+		btnRemoverClien.setBounds(141, 175, 89, 23);
+		panel_RmvCliente.add(btnRemoverClien);
+		
+		final Panel panel_RmvProd = new Panel();
+		panel_RmvProd.setLayout(null);
+		panel_RmvProd.setBackground(Color.CYAN);
+		panel_RmvProd.setBounds(0, 0, 358, 235);
+		panel.add(panel_RmvProd);
+		
+		tf_RmvProdutoID = new JTextField();
+		tf_RmvProdutoID.setBounds(123, 100, 128, 20);
+		panel_RmvProd.add(tf_RmvProdutoID);
+		tf_RmvProdutoID.setColumns(10);
+		
+		JLabel lblNome = new JLabel("ID");
+		lblNome.setBounds(91, 103, 22, 14);
+		panel_RmvProd.add(lblNome);
+		
+		JLabel lblDigiteAbaixoO = new JLabel("Digite abaixo o ID do produto que deseja remover");
+		lblDigiteAbaixoO.setBounds(63, 29, 240, 14);
+		panel_RmvProd.add(lblDigiteAbaixoO);
+		
+		JButton btnRemoverProd = new JButton("Remover");
+		btnRemoverProd.setBounds(141, 175, 89, 23);
+		panel_RmvProd.add(btnRemoverProd);
 		
 		final Panel panel_CadastroGul = new Panel();
 		panel_CadastroGul.setBounds(0, 0, 358, 235);
@@ -148,30 +245,81 @@ public class TelaAdmin extends JFrame {
 		});
 		btnCadastrar.setBounds(142, 205, 89, 23);
 		panel_CadastroGul.add(btnCadastrar);
-		Panel panel_AttCliente = new Panel();
-		panel_AttCliente.setBounds(-10241, -10171, 358, 164);
-		panel.add(panel_AttCliente);
-		panel_AttCliente.setBackground(Color.CYAN);
-		panel_AttCliente.setLayout(null);
 		
 		JButton btnRemoverProdutos = new JButton("Remover Produtos");
+		btnRemoverProdutos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_RmvProd);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
 		btnRemoverProdutos.setBounds(10, 157, 180, 23);
 		jPanel1.add(btnRemoverProdutos);
 		
 		JButton btnRemoverCliente = new JButton("Remover Cliente");
-		btnRemoverCliente.setBounds(10, 191, 180, 23);
+		btnRemoverCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_RmvCliente);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
+		btnRemoverCliente.setBounds(10, 190, 180, 23);
 		jPanel1.add(btnRemoverCliente);
 		
 		JButton btnAtualizarCliente = new JButton("Atualizar Cliente");
-		btnAtualizarCliente.setBounds(10, 225, 180, 23);
+		btnAtualizarCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_AttCliente);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
+		btnAtualizarCliente.setBounds(10, 223, 180, 23);
 		jPanel1.add(btnAtualizarCliente);
 		
-		JButton btnNewButton = new JButton("Estoque");
-		btnNewButton.setBounds(10, 259, 180, 23);
-		jPanel1.add(btnNewButton);
+		JButton btnAtualizarProduto = new JButton("Atualizar Produto");
+		btnAtualizarProduto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_AttProduto);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
+		btnAtualizarProduto.setBounds(11, 257, 180, 23);
+		jPanel1.add(btnAtualizarProduto);
+		
+		JButton btnEstoque = new JButton("Estoque");
+		btnEstoque.setBounds(10, 289, 180, 23);
+		jPanel1.add(btnEstoque);
 		
 		JButton btnGerarRelatorios = new JButton("Gerar Relatorios");
-		btnGerarRelatorios.setBounds(10, 293, 180, 23);
+		btnGerarRelatorios.setBounds(10, 321, 180, 23);
 		jPanel1.add(btnGerarRelatorios);
 		
 		JButton btnCadastrarProdutos = new JButton("Cadastrar Produtos");
@@ -192,7 +340,7 @@ public class TelaAdmin extends JFrame {
 		jPanel1.add(btnCadastrarProdutos);
 		
 		
-		JLabel lblBackground = new JLabel("New label");
+		JLabel lblBackground = new JLabel("");
 		lblBackground.setBounds(0, 0, 624, 428);
 		lblBackground.setIcon(new ImageIcon("images\\Fundo.jpg"));
 		jPanel1.add(lblBackground);
