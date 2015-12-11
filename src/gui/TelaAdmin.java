32f31284d7c6e5e51e0ce0a726fe48c8a3e7905b
rlo <rlo@G1C34.windows.cin.ufpe.br>
2015-12-11 20:23:45 -0300

    Mudanças

--- src/gui/TelaAdmin.java
+++ src/gui/TelaAdmin.java
@@ -24,9 +24,16 @@ import java.awt.Color;
 
 import javax.swing.JInternalFrame;
 
+import negocios.classesBasicas.Cliente;
+import negocios.classesBasicas.Endereco;
 import negocios.classesBasicas.Guloseimas;
 import negocios.classesBasicas.Produto;
+import negocios.classesBasicas.Travessuras;
+import negocios.exceptions.ClienteJaExisteException;
+import negocios.exceptions.ClienteNaoEncontradoException;
+import negocios.exceptions.EmptyListException;
 import negocios.exceptions.ProdutoJaExisteException;
+import negocios.exceptions.ProdutoNaoEncontradoException;
 
 import java.awt.Panel;
 import java.awt.Label;
@@ -86,17 +93,23 @@ public class TelaAdmin extends JFrame {
 		panel.setBounds(233, 100, 358, 235);
 		jPanel1.add(panel);
 		panel.setVisible(false);
-		panel.setLayout(null);
+		panel.setLayout(new CardLayout(0, 0));
+		
+		final Panel panel_Escolha = new Panel();
+		panel_Escolha.setLayout(null);
+		panel_Escolha.setBackground(new Color(222, 184, 135));
+		panel.add(panel_Escolha, "name_84949386013763");
+		
+		
 		
 		final Panel panel_Trav = new Panel();
 		panel_Trav.setLayout(null);
 		panel_Trav.setBackground(new Color(222, 184, 135));
-		panel_Trav.setBounds(0, 0, 358, 235);
-		panel.add(panel_Trav);
+		panel.add(panel_Trav, "name_84949404525996");
 		
-		TextField textField = new TextField();
-		textField.setBounds(86, 10, 240, 22);
-		panel_Trav.add(textField);
+		final TextField tf_NomeT = new TextField();
+		tf_NomeT.setBounds(86, 10, 240, 22);
+		panel_Trav.add(tf_NomeT);
 		
 		Label label_8 = new Label("Nome");
 		label_8.setForeground(Color.WHITE);
@@ -104,9 +117,9 @@ public class TelaAdmin extends JFrame {
 		label_8.setBounds(10, 10, 76, 24);
 		panel_Trav.add(label_8);
 		
-		TextField textField_1 = new TextField();
-		textField_1.setBounds(86, 49, 240, 22);
-		panel_Trav.add(textField_1);
+		final TextField tf_CodigoT = new TextField();
+		tf_CodigoT.setBounds(86, 49, 240, 22);
+		panel_Trav.add(tf_CodigoT);
 		
 		Label label_9 = new Label("C\u00F3digo");
 		label_9.setForeground(Color.WHITE);
@@ -114,9 +127,9 @@ public class TelaAdmin extends JFrame {
 		label_9.setBounds(10, 47, 76, 24);
 		panel_Trav.add(label_9);
 		
-		TextField textField_2 = new TextField();
-		textField_2.setBounds(86, 86, 240, 22);
-		panel_Trav.add(textField_2);
+		final TextField tf_DesT = new TextField();
+		tf_DesT.setBounds(86, 86, 240, 22);
+		panel_Trav.add(tf_DesT);
 		
 		Label label_10 = new Label("Descri\u00E7\u00E3o");
 		label_10.setForeground(Color.WHITE);
@@ -124,11 +137,11 @@ public class TelaAdmin extends JFrame {
 		label_10.setBounds(10, 84, 76, 24);
 		panel_Trav.add(label_10);
 		
-		TextField textField_3 = new TextField();
-		textField_3.setBounds(86, 124, 240, 22);
-		panel_Trav.add(textField_3);
+		final TextField tf_PrecoT = new TextField();
+		tf_PrecoT.setBounds(86, 124, 240, 22);
+		panel_Trav.add(tf_PrecoT);
 		
-		Choice tf_Censura = new Choice();
+		final Choice tf_Censura = new Choice();
 		
 		for(int i = 0; i < 19; i++){
 		tf_Censura.add(Integer.toString(i));
@@ -136,7 +149,7 @@ public class TelaAdmin extends JFrame {
 		tf_Censura.setBounds(85, 161, 68, 22);
 		panel_Trav.add(tf_Censura);
 		
-		Choice tf_Peri = new Choice();
+		final Choice tf_Peri = new Choice();
 		for(int i = 3; i < 11; i++){
 			tf_Peri.add(Integer.toString(i));
 		}
@@ -162,16 +175,56 @@ public class TelaAdmin extends JFrame {
 		panel_Trav.add(label_12);
 		
 		JButton button = new JButton("Cadastrar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				cadastrarTrav(tf_NomeT.getText(), tf_CodigoT.getText(), tf_DesT.getText(), Double.parseDouble(tf_Peri.getSelectedItem()), Integer.parseInt(tf_Censura.getSelectedItem()), Double.parseDouble(tf_PrecoT.getText()));
+				JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+			}
+		});
 		button.setBounds(127, 205, 104, 23);
 		panel_Trav.add(button);
 		
 		
 		final Panel panel_CadastroGul = new Panel();
-		panel_CadastroGul.setBounds(0, 0, 358, 235);
-		panel.add(panel_CadastroGul);
+		panel.add(panel_CadastroGul, "name_84949424298320");
 		panel_CadastroGul.setBackground(new Color(222, 184, 135));
 		panel_CadastroGul.setLayout(null);
 		
+		JButton btnTravessuras = new JButton("Travessuras");
+		btnTravessuras.setIcon(new ImageIcon("images\\botton.png"));
+		btnTravessuras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_Trav);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
+		JButton btnGuloseimas = new JButton("Guloseimas");
+		btnGuloseimas.setIcon(new ImageIcon("images\\botton.png"));
+		btnGuloseimas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panel.setVisible(true);
+				// Limpando
+				panel.removeAll();
+				panel.repaint();
+				panel.revalidate();
+				// Adicionando novo panel
+				panel.add(panel_CadastroGul);
+				panel.repaint();
+				panel.revalidate();
+			}
+		});
+		btnGuloseimas.setBounds(10, 69, 164, 85);
+		panel_Escolha.add(btnGuloseimas);
+		
+		btnTravessuras.setBounds(184, 70, 164, 85);
+		panel_Escolha.add(btnTravessuras);
 		final TextField tf_NomeCad = new TextField();
 		tf_NomeCad.setBounds(86, 10, 240, 22);
 		panel_CadastroGul.add(tf_NomeCad);
@@ -224,26 +277,19 @@ public class TelaAdmin extends JFrame {
 				
 				
 				
-				JButton btnCadastrar = new JButton("Cadastrar");
-				btnCadastrar.addActionListener(new ActionListener() {
+				JButton btnCadastrarG = new JButton("Cadastrar");
+				btnCadastrarG.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
 						cadastrarGuloseima(tf_NomeCad.getText(), tf_CodCad.getText(), tf_DescCad.getText(), tf_SaborCad.getText(), Double.parseDouble(tf_PrecoCad.getText()));
 					}
 				});
-				btnCadastrar.setBounds(127, 205, 104, 23);
-				panel_CadastroGul.add(btnCadastrar);
-		
-		final Panel panel_Escolha = new Panel();
-		panel_Escolha.setLayout(null);
-		panel_Escolha.setBackground(new Color(222, 184, 135));
-		panel_Escolha.setBounds(0, 0, 358, 235);
-		panel.add(panel_Escolha);
+				btnCadastrarG.setBounds(127, 205, 104, 23);
+				panel_CadastroGul.add(btnCadastrarG);
 		
 		final Panel panel_AttProduto = new Panel();
 		panel_AttProduto.setLayout(null);
 		panel_AttProduto.setBackground(new Color(222, 184, 135));
-		panel_AttProduto.setBounds(0, 0, 358, 235);
-		panel.add(panel_AttProduto);
+		panel.add(panel_AttProduto, "name_84949443447977");
 		
 		tf_AttProdID = new JTextField();
 		tf_AttProdID.setColumns(10);
@@ -269,46 +315,10 @@ public class TelaAdmin extends JFrame {
 		});
 		btnAttProd.setBounds(141, 175, 89, 23);
 		panel_AttProduto.add(btnAttProd);
-		
-		JButton btnTravessuras = new JButton("Travessuras");
-		btnTravessuras.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				panel.setVisible(true);
-				// Limpando
-				panel.removeAll();
-				panel.repaint();
-				panel.revalidate();
-				// Adicionando novo panel
-				panel.add(panel_Trav);
-				panel.repaint();
-				panel.revalidate();
-			}
-		});
-		
-		btnTravessuras.setBounds(204, 70, 124, 85);
-		panel_Escolha.add(btnTravessuras);
-		
-		JButton btnGuloseimas = new JButton("Guloseimas");
-		btnGuloseimas.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				panel.setVisible(true);
-				// Limpando
-				panel.removeAll();
-				panel.repaint();
-				panel.revalidate();
-				// Adicionando novo panel
-				panel.add(panel_CadastroGul);
-				panel.repaint();
-				panel.revalidate();
-			}
-		});
-		btnGuloseimas.setBounds(30, 69, 124, 85);
-		panel_Escolha.add(btnGuloseimas);
 		final Panel panel_AttCliente = new Panel();
 		panel_AttCliente.setLayout(null);
 		panel_AttCliente.setBackground(new Color(222, 184, 135));
-		panel_AttCliente.setBounds(0, 0, 358, 235);
-		panel.add(panel_AttCliente);
+		panel.add(panel_AttCliente, "name_84949463829495");
 		
 		tf_AttClienteID = new JTextField();
 		tf_AttClienteID.setColumns(10);
@@ -326,16 +336,46 @@ public class TelaAdmin extends JFrame {
 		lblDigiteAbaixoO_2.setForeground(new Color(255, 255, 255));
 		lblDigiteAbaixoO_2.setBounds(31, 32, 301, 20);
 		panel_AttCliente.add(lblDigiteAbaixoO_2);
+		Endereco endereco = new Endereco("Hogsmeade", "Rua dos Alfeneiros","4","00000000", "Onde o vento faz a curva");
+		try {
+			GemialidadesLoja.getInstance().inserirCliente(new Cliente("Rodrigo", "15", endereco, "552", "552"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClienteJaExisteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		JButton btnAttClien = new JButton("Atualizar");
+		btnAttClien.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			    Cliente cliente = null;
+				try {
+					cliente = GemialidadesLoja.getInstance().procurarCliente(tf_AttClienteID.getText());
+				} catch (FileNotFoundException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				} catch (ClienteNaoEncontradoException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				} catch (EmptyListException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
+				TelaAtualizarCliente tela = new TelaAtualizarCliente(loja, cliente);
+				tela.setVisible(true);
+			}
+		});
 		btnAttClien.setBounds(141, 175, 89, 23);
 		panel_AttCliente.add(btnAttClien);
 		
 		final Panel panel_RmvCliente = new Panel();
 		panel_RmvCliente.setLayout(null);
 		panel_RmvCliente.setBackground(new Color(222, 184, 135));
-		panel_RmvCliente.setBounds(0, 0, 358, 235);
-		panel.add(panel_RmvCliente);
+		panel.add(panel_RmvCliente, "name_84949484452251");
 		
 		tf_RmvClienteID = new JTextField();
 		tf_RmvClienteID.setColumns(10);
@@ -355,14 +395,19 @@ public class TelaAdmin extends JFrame {
 		panel_RmvCliente.add(lblDigiteAbaixoO_1);
 		
 		JButton btnRemoverClien = new JButton("Remover");
+		btnRemoverClien.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				removerCli(tf_RmvClienteID.getText());
+				
+			}
+		});
 		btnRemoverClien.setBounds(141, 175, 89, 23);
 		panel_RmvCliente.add(btnRemoverClien);
 		
 		final Panel panel_RmvProd = new Panel();
 		panel_RmvProd.setLayout(null);
 		panel_RmvProd.setBackground(new Color(222, 184, 135));
-		panel_RmvProd.setBounds(0, 0, 358, 235);
-		panel.add(panel_RmvProd);
+		panel.add(panel_RmvProd, "name_84949505404467");
 		
 		tf_RmvProdutoID = new JTextField();
 		tf_RmvProdutoID.setBounds(123, 100, 128, 20);
@@ -382,6 +427,11 @@ public class TelaAdmin extends JFrame {
 		panel_RmvProd.add(lblDigiteAbaixoO);
 		
 		JButton btnRemoverProd = new JButton("Remover");
+		btnRemoverProd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				removerProd(tf_RmvProdutoID.getText());
+			}
+		});
 		btnRemoverProd.setBounds(141, 175, 89, 23);
 		panel_RmvProd.add(btnRemoverProd);
 		
@@ -494,6 +544,45 @@ public class TelaAdmin extends JFrame {
 		} catch (ProdutoJaExisteException e) {
 			JOptionPane.showMessageDialog(this, e.getMessage());
 		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		}
+	}
+	public void cadastrarTrav(String nome, String codigo, String descricao, double periculosidade, int censura, double preço){
+		Produto produto = new Travessuras(nome, codigo, descricao, periculosidade, censura, preço);
+		try {
+			GemialidadesLoja.getInstance().cadastrarProduto(produto);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (ProdutoJaExisteException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		}
+	}
+	public void removerProd(String codigo){
+		try {
+			GemialidadesLoja.getInstance().removerProduto(codigo);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (ProdutoNaoEncontradoException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (EmptyListException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());	
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+
+		}
+	}
+	public void removerCli(String id){
+		try {
+			GemialidadesLoja.getInstance().removerCliente(id);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (ClienteNaoEncontradoException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (EmptyListException e) {
 			JOptionPane.showMessageDialog(this, e.getMessage());
 		}
 	}
---
+++ src/gui/TelaAtualizarCliente.java
@@ -0,0 +1,257 @@
+
+package gui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import negocios.classesBasicas.Cliente;
+import negocios.classesBasicas.Endereco;
+import negocios.exceptions.ClienteJaExisteException;
+import negocios.exceptions.ClienteNaoEncontradoException;
+import negocios.exceptions.EmptyListException;
+import fachada.GemialidadesLoja;
+
+public class TelaAtualizarCliente extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tf_LogradouroA;
+	private JTextField tf_CEPA;
+	private JTextField tf_NumeroA;
+	private JTextField tf_ComplementoA;
+	private JPasswordField passwordFieldA;
+	private static GemialidadesLoja loja;
+	private JTextField tf_NomeA;
+	private JTextField tf_IDADEA;
+	private JTextField tf_CidadeA;
+	private static Cliente cliente;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaAtualizarCliente frame = new TelaAtualizarCliente(loja, cliente);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaAtualizarCliente(GemialidadesLoja loja, final Cliente cliente) {
+		this.loja = loja;
+		this.cliente = cliente;
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 642, 482);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnCadastrar = new JButton("Atualizar");
+		btnCadastrar.setBounds(262, 374, 101, 30);
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String id = cliente.getId();
+				Endereco endereco = new Endereco(tf_CidadeA.getText(), tf_LogradouroA.getText(), tf_NumeroA.getText(), tf_CEPA.getText(), tf_ComplementoA.getText());
+				
+					atualizar(tf_NomeA.getText(), tf_IDADEA.getText(), endereco, id, passwordFieldA.getText());
+					JOptionPane.showMessageDialog(null, "Atualização efetuada com sucesso");       
+					setVisible(false);
+
+			}}
+				);
+
+		contentPane.add(btnCadastrar);
+
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblSenha.setForeground(Color.ORANGE);
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSenha.setBounds(10, 259, 36, 14);
+		contentPane.add(lblSenha);
+
+		JLabel lblEndereo = new JLabel("Endere\u00E7o");
+		lblEndereo.setForeground(Color.ORANGE);
+		lblEndereo.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblEndereo.setBounds(440, 106, 89, 23);
+		contentPane.add(lblEndereo);
+
+		JLabel lblDados = new JLabel("Dados Pessoais");
+		lblDados.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDados.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblDados.setForeground(Color.ORANGE);
+		lblDados.setBounds(26, 107, 217, 20);
+		contentPane.add(lblDados);
+
+		JLabel lblCidade = new JLabel("Cidade\r\n");
+		lblCidade.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblCidade.setForeground(Color.ORANGE);
+		lblCidade.setBackground(Color.ORANGE);
+		lblCidade.setBounds(276, 194, 96, 17);
+		contentPane.add(lblCidade);
+
+		JLabel lblLogradouro = new JLabel("Endere\u00E7o");
+		lblLogradouro.setForeground(Color.ORANGE);
+		lblLogradouro.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblLogradouro.setBounds(276, 227, 96, 17);
+		contentPane.add(lblLogradouro);
+
+		JLabel lblCEP = new JLabel("CEP");
+		lblCEP.setForeground(Color.ORANGE);
+		lblCEP.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblCEP.setBounds(276, 259, 96, 14);
+		contentPane.add(lblCEP);
+
+		JLabel lblNumero = new JLabel("Numero");
+		lblNumero.setForeground(Color.ORANGE);
+		lblNumero.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNumero.setBounds(276, 290, 96, 14);
+		contentPane.add(lblNumero);
+
+		JLabel lblComplemento = new JLabel("Complemento");
+		lblComplemento.setForeground(Color.ORANGE);
+		lblComplemento.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblComplemento.setBounds(276, 315, 96, 14);
+		contentPane.add(lblComplemento);
+
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNome.setForeground(Color.ORANGE);
+		lblNome.setBounds(10, 193, 36, 14);
+		contentPane.add(lblNome);
+
+		JLabel lblIdade = new JLabel("Idade");
+		lblIdade.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblIdade.setForeground(Color.ORANGE);
+		lblIdade.setBounds(10, 228, 36, 14);
+		contentPane.add(lblIdade);
+
+		tf_LogradouroA = new JTextField();
+		tf_LogradouroA.setBounds(379, 226, 217, 20);
+		contentPane.add(tf_LogradouroA);
+		tf_LogradouroA.setColumns(10);
+		tf_LogradouroA.setText(cliente.getEndereco().getLogradouro());
+
+		tf_CEPA = new JTextField();
+		tf_CEPA.setBounds(379, 257, 150, 20);
+		contentPane.add(tf_CEPA);
+		tf_CEPA.setColumns(10);
+		tf_CEPA.setText(cliente.getEndereco().getCep());
+
+		tf_NumeroA = new JTextField();
+		tf_NumeroA.setBounds(379, 284, 86, 20);
+		contentPane.add(tf_NumeroA);
+		tf_NumeroA.setColumns(10);
+		tf_NumeroA.setText(cliente.getEndereco().getNumero());
+
+		tf_ComplementoA = new JTextField();
+		tf_ComplementoA.setBounds(379, 313, 217, 20);
+		contentPane.add(tf_ComplementoA);
+		tf_ComplementoA.setColumns(10);
+		tf_ComplementoA.setText(cliente.getEndereco().getComplemento());
+
+		tf_CidadeA = new JTextField();
+		tf_CidadeA.setBounds(379, 191, 217, 20);
+		contentPane.add(tf_CidadeA);
+		tf_CidadeA.setColumns(10);
+		tf_CidadeA.setText(cliente.getEndereco().getCidade());
+		
+		tf_NomeA = new JTextField();
+		tf_NomeA.setBounds(56, 191, 177, 20);
+		contentPane.add(tf_NomeA);
+		tf_NomeA.setColumns(10);
+		tf_NomeA.setText(cliente.getNome());
+
+		tf_IDADEA = new JTextField();
+		tf_IDADEA.setBounds(56, 222, 61, 20);
+		contentPane.add(tf_IDADEA);
+		tf_IDADEA.setColumns(10);
+		tf_IDADEA.setText(cliente.getIdade());
+
+
+		passwordFieldA = new JPasswordField();
+		passwordFieldA.setBounds(56, 257, 109, 20);
+		contentPane.add(passwordFieldA);
+		passwordFieldA.setText(cliente.getSenha());
+
+		JLabel lblbastao4 = new JLabel("");
+		lblbastao4.setIcon(new ImageIcon("images\\2.2.png"));
+		lblbastao4.setBounds(0, 113, 61, 16);
+		contentPane.add(lblbastao4);
+
+		JLabel lblbastao2 = new JLabel("");
+		lblbastao2.setIcon(new ImageIcon("images\\1.1.png"));
+		lblbastao2.setBounds(205, 115, 96, 14);
+		contentPane.add(lblbastao2);
+
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon("images\\equerda.png"));
+		label_1.setBounds(342, 113, 88, 14);
+		contentPane.add(label_1);
+
+		JLabel lblbastao1 = new JLabel("");
+		lblbastao1.setIcon(new ImageIcon("images\\direita.png"));
+		lblbastao1.setBounds(526, 113, 90, 14);
+		contentPane.add(lblbastao1);
+
+		JLabel lblCoruja = new JLabel("");
+		lblCoruja.setIcon(new ImageIcon("images\\topo.png"));
+		lblCoruja.setBounds(208, 11, 224, 65);
+		contentPane.add(lblCoruja);
+
+		JLabel lblBackground = new JLabel("");
+		lblBackground.setBackground(Color.GRAY);
+		lblBackground.setIcon(new ImageIcon("images\\Fundo.jpg"));
+		lblBackground.setBounds(0, 0, 626, 443);
+		contentPane.add(lblBackground);
+
+
+	}
+	public Endereco cadastrarEndereco(String cidade, String logradouro, String numero, String cep, String complemento){
+		Endereco endereco = new Endereco(cidade, logradouro, numero, cep, complemento);
+		return endereco;
+
+	}
+	public boolean atualizar(String nome, String idade, Endereco endereco, String id, String senha) {
+		Cliente cliente = new Cliente( nome, idade, endereco, id, senha);
+		try {
+			GemialidadesLoja.getInstance().atualizarCliente(id,cliente);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		} catch (ClienteNaoEncontradoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EmptyListException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		return rootPaneCheckingEnabled;
+	}
+}
